4abb9be5f5fc2b7c5ec4686abd415100a35ddd23
loyladejito <19104867@usc.edu.ph>
2021-04-20 20:33:17 +0800

    initial commit

--- src/javaapp/JavaCollectionsandFrameworks.java
+++ src/javaapp/JavaCollectionsandFrameworks.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class JavaMap {
+public class JavaCollectionsandFrameworks {
 
     public static void main(String[] args) {
         number1();
