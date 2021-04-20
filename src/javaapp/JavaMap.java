8fb442e23f865de0dc05ad021cc9f82f6830bd09
loyladejito <19104867@usc.edu.ph>
2021-04-20 20:29:44 +0800

    initial commit

--- src/javaapp/JavaMap.java
+++ src/javaapp/JavaMap.java
@@ -1,4 +1,5 @@
 package javaapp;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class JavaMap {
+
     public static void main(String[] args) {
         number1();
         number2();
@@ -18,7 +20,7 @@ public class JavaMap {
         number7();
         number8();
     }
-    
+
     public static void number1() {
         List<String> nameList = new ArrayList<String>();
         nameList.add("Aestherielle");
@@ -26,52 +28,52 @@ public class JavaMap {
         nameList.add("Thraia");
         nameList.add("Klare");
         nameList.add("Eurydice");
-        
+
         System.out.println("\t' 1.SORTING A GIVEN ARRAY LIST '");
         Collections.sort(nameList);
         System.out.println("Sorted Array Ascending: \n\t" + nameList);
 
         Collections.sort(nameList, Collections.reverseOrder());
         System.out.println("\nSorted Array Descending: \n\t" + nameList);
-        
+
         System.out.println("\n    ---------------------------------------------\n");
     }
 
-    public static void number2(){
+    public static void number2() {
         List<String> nameList = new ArrayList<String>();
         nameList.add("Zamiel");
         nameList.add("Josiah");
         nameList.add("Lienzo");
         nameList.add("Elijah");
         nameList.add("Vincent");
-        
+
         System.out.println("\t' 2.RANDOM ARRAY SORTING '");
         Collections.shuffle(nameList);
         System.out.println("Random Sorted Array: \n\t" + nameList);
-        
+
         System.out.println("\n    ---------------------------------------------\n");
     }
-    
-    public static void number3(){
-        
+
+    public static void number3() {
+
         System.out.println("\t' 3.MOVING MINIMUM VALUE OF LIST TO THE FRONT '");
-        
+
         System.out.println("\n    ---------------------------------------------\n");
     }
-    
-    public static void number4(){
-        
+
+    public static void number4() {
+
         System.out.println("\t' 4.isEmpty() VS size '");
-        
+
         System.out.println("    The following are the reasons why we should opt for isEmpty() than size():\n"
                 + "\t~ isEmpty() is more expressive (the code's easier to read and maintain);\n"
                 + "\t~ faster, in some cases by orders of magnitude");
-        
+
         System.out.println("\n    ---------------------------------------------\n");
     }
-    
-    public static void number5(){
-        
+
+    public static void number5() {
+
         System.out.println("\t' 5.COMPARE AND CONTRAST CLASSIC for loop VS foreach '");
         System.out.println("COMPARISON:\n"
                 + "     for loop:\n"
@@ -113,19 +115,19 @@ public class JavaMap {
                 + "\t   ~ Cannot retrieve elements based on an index;\n"
                 + "\t   ~ It is not possible to traverse in reverse order;\n"
                 + "\t   ~ Cannot modify array element values\n");
-                
+
         System.out.println("\n    ---------------------------------------------\n");
     }
 
-     public static void number6() {
+    public static void number6() {
         System.out.println("\t' 6.COMPARE AND RETURN COMMON VALUES OF 2 SETS '");
-        
+
         Set<String> arr_set1 = new HashSet<String>();
         arr_set1.add("One");
         arr_set1.add("Two");
         arr_set1.add("Three");
         arr_set1.add("Four");
-        
+
         System.out.println("First Set: " + arr_set1);
 
         Set<String> arr_set2 = new HashSet<String>();
@@ -133,27 +135,27 @@ public class JavaMap {
         arr_set2.add("Two");
         arr_set2.add("Six");
         arr_set2.add("Four");
-        
+
         System.out.println("Second Set: " + arr_set2);
-        
+
         Set<String> intersection = new HashSet<String>(arr_set1);
         arr_set1.retainAll(arr_set2);
         intersection.retainAll(arr_set2);
 
-        System.out.println("First Set that intersects Second Set: " + intersection);
-        
+        System.out.println("Common values of 2 sets: " + intersection);
+
         System.out.println("\n    ---------------------------------------------\n");
     }
 
     public static void number7() {
         System.out.println("\t' 7.COMPARE AND RETURN UNIQUE VALUES BETWEEN 2 SETS '");
-        
+
         Set<Integer> arr_set1 = new HashSet<Integer>();
         arr_set1.add(10);
         arr_set1.add(20);
         arr_set1.add(30);
         arr_set1.add(40);
-        
+
         System.out.println("First Set: " + arr_set1);
 
         Set<Integer> arr_set2 = new HashSet<Integer>();
@@ -162,7 +164,7 @@ public class JavaMap {
         arr_set2.add(50);
         arr_set2.add(60);
         arr_set2.add(10);
-        
+
         System.out.println("Second Set: " + arr_set2);
 
         Set<Integer> result = new HashSet<Integer>(arr_set1);
@@ -171,22 +173,21 @@ public class JavaMap {
         result2.removeAll(arr_set1);
 
         result.addAll(result2);
-        System.out.println("First Set that intersects Second Set: " + result);
-        
+        System.out.println("Different values between 2 sets: " + result);
+
         System.out.println("\n    ---------------------------------------------\n");
     }
-    
+
     public static void number8() {
         System.out.println("\t' 8.COUNT NUMBER KEYS IN A MAP '");
-        
+
         Map<String, String> keymap = new HashMap<String, String>();
         keymap.put("concordia", "Value given");
         keymap.put("acconcordia", "Test");
         keymap.put("condensada", "Sweet");
         System.out.println("Number of keys in a map: " + keymap.size());
-        
+
         System.out.println("\n    ---------------------------------------------\n");
     }
 
-   
 }
